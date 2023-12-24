c69003db804c69b1a1ae7c4b8be98bb40d2a5a57
nmduonggg <nmduong2104@gmail.com>
2023-12-24 14:16:05 +0700

    update

--- scripts/infer/infer_1.sh
+++ scripts/infer/infer_1.sh
@@ -1,6 +1,6 @@
 python infer.py \
     --task 'infer_1' \
-    --weights './outputs/train_baseline_stn0_augment/best_cer.pth' \
+    --weights './weight.pth' \
     --img-width 100 \
     --img-height 32 \
     --max-length 25 \
